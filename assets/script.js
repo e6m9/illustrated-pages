a0c79ced870fa3da0712cbe24dadc3e8e7c8fa62
Mona Lisa <147671781+ajcasablanca@users.noreply.github.com>
2023-11-27 21:04:01 -0500

    created marvel api

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,12 @@
-var time = new Date().toTimeString()
+var time = new Date().getTime()
 console.log(time)
-var hash = CryptoJS.MD5(time + "af2bfa5e8b8999a6887a51f054f4bc539e814945" + "b5cfe2a57cc83be0cb757b07557c487e").toString();
+
+var hash = CryptoJS.MD5( time + "af2bfa5e8b8999a6887a51f054f4bc539e814945" + "b5cfe2a57cc83be0cb757b07557c487e").toString();
 console.log(hash)
-fetch("https://gateway.marvel.com/v1/public/characters?ts=" + time +  "&apikey=b5cfe2a57cc83be0cb757b07557c487e&hash=" + hash)
+
+// fetch("https://gateway.marvel.com/v1/public/characters?ts=" + time +  "&apikey=b5cfe2a57cc83be0cb757b07557c487e&hash=" + hash)
+fetch(`http://gateway.marvel.com/v1/public/comics?ts=${time}&apikey=b5cfe2a57cc83be0cb757b07557c487e
+&hash=af2bfa5e8b8999a6887a51f054f4bc539e814945`)
 .then(response => {
     return response.json()
 }).then(data => {
