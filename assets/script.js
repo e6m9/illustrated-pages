fabc830c35b970e22c1968079550b26118702281
e6m9 <alostsound@gmail.com>
2023-11-27 19:22:19 -0500

    marvel api code

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+var time = new Date().toTimeString()
+console.log(time)
+var hash = CryptoJS.MD5( time + "af2bfa5e8b8999a6887a51f054f4bc539e814945" + "b5cfe2a57cc83be0cb757b07557c487e").toString();
+console.log(hash)
+fetch("https://gateway.marvel.com/v1/public/characters?ts=" + time +  "&apikey=b5cfe2a57cc83be0cb757b07557c487e&hash=" + hash)
+.then(response => {
+    return response.json()
+}).then(data => {
+    console.log(data);
+})
