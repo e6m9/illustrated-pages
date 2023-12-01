a42815a5bef1263a10e9c6c0f6dd36a6bb0194ca
Mona Lisa <147671781+ajcasablanca@users.noreply.github.com>
2023-11-30 20:34:09 -0500

    updated code

--- assets/script.js
+++ assets/script.js
@@ -4,18 +4,7 @@ console.log(time)
 var hash = CryptoJS.MD5(time + "af2bfa5e8b8999a6887a51f054f4bc539e814945" + "b5cfe2a57cc83be0cb757b07557c487e").toString();
 console.log(hash)
 
-function getParams() {
-    // Get the search params out of the URL (i.e. `?q=london&format=photo`) and convert it to an array (i.e. ['?q=london', 'format=photo'])
-    var searchParamsArr = document.location.search.split('&');
-  
-    // Get the query and format values
-    var query = searchParamsArr[0].split('=').pop();
-    var format = searchParamsArr[1].split('=').pop();
-  
-    marvelApi(query, format);
-  }
-
-function marvelAPI(query, format) {
+function marvelAPI() {
 
     fetch("https://gateway.marvel.com/v1/public/characters?ts=" + time + "&apikey=b5cfe2a57cc83be0cb757b07557c487e&hash=" + hash)
         // fetch(`http://gateway.marvel.com/v1/public/comics?ts=${time}&apikey=b5cfe2a57cc83be0cb757b07557c487e&hash=af2bfa5e8b8999a6887a51f054f4bc539e814945`)
@@ -23,18 +12,31 @@ function marvelAPI(query, format) {
             return response.json()
         })
         .then(locRes => {
-            resultTextEl.textContent = locRes.search.query;
+            // resultTextEl.textContent = locRes.search.query;
             console.log(locRes);
+            var results = locRes.data.results
 
-            resultContentEl.textContent = "";
-            for (var i = 0; i < locRes.results.length; i++) {
-                printResults(locRes.results[i]);
+            // resultContentEl.textContent = "";
+            for (var i = 0; i < results.length; i++) {
+                console.log(results[i]);
+                var name = results[i].name;
+                var description = results[i].description;
+                var thumbnail = results[i].thumbnail.path + '.' + results[i].thumbnail.extension;
+                var nameContentEl = document.createElement('h3');
+                var descriptionContentEl = document.createElement('P');
+                var thumbnailImgEl = document.createElement('img');
+                var textBody = document.getElementById('textBox');
+                textBody.appendChild(nameContentEl);
+                nameContentEl.innerText = "name: " + name;
+                textBody.appendChild(descriptionContentEl);
+                descriptionContentEl.innerText = "description: " + description;
+                textBody.appendChild(thumbnailImgEl);
+                thumbnailImgEl.src = thumbnail;
+                thumbnailImgEl.style.width='50%';
+
             }
+
         })
-
-
 }
 
-
-
-
+marvelAPI()
